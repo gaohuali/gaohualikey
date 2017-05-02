f2802a1eabc5ec7e11af4fd49d182a205fe5f2c2
qingyun <qingyun@gaohuali.local>
2017-05-02 15:14:25 +0800

    b.js

---
+++ b.js
@@ -0,0 +1,15 @@
+<script type="text/javascript" src='js/jq.js'></script>
+	<script type="text/javascript" src='js/myfocus-2.0.1.min.js'></script>
+	<script type="text/javascript">
+		myFocus.set({
+			id:'boxID',
+			pattern:'mF_fancy',
+			width:520,
+			height:280
+			
+
+
+		})
+	</script>
+
+
